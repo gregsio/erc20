8e37e1bca19a69136c2e7152c1f2d244d01870d7
gregs <github@gregs.io>
2023-06-23 15:43:33 +0200

    Deploys CZG tokens to testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+const ALCHEMY_API_KEY = process.env.ALCHEMY_SEPOLIA_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    localhost: {}
+    localhost: {},
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
   },
 };
--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,11 +1,10 @@
 const hre = require("hardhat");
-
 async function main() {
     // fetch conctract to deploy
     const Token = await ethers.getContractFactory('Token')   
 
     //deploy contract
-    const token = await Token.deploy()
+    const token = await Token.deploy('SYZYGY','CZG', 1000000)
     await token.deployed()
     console.log(`Token deployed at: ${token.address}`)
 }
